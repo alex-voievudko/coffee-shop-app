b86cbc8c0577324850807a24c03bd99ba34f4bc2
Alex Voievudko <alex.voievudko@gmail.com>
2023-12-29 11:42:29 +0100

    feat: add theme
    # type feat fix docs style refactor test chore

---
+++ @types/theme.ts
@@ -0,0 +1,14 @@
+export interface Theme {
+	fonts: {
+		regular: string;
+		semibold: string;
+	};
+	colors: {
+		white: string;
+		black: string;
+		primary: string;
+		primaryDark: string;
+		textPrimary: string;
+		textSecondary: string;
+	};
+}
---
+++ theme/index.ts
@@ -0,0 +1,16 @@
+import { Theme } from '~/@types/theme';
+
+export const theme: Theme = {
+	fonts: {
+		regular: 'Sora_400Regular',
+		semibold: 'Sora_600SemiBold',
+	},
+	colors: {
+		white: '#FFFFFF',
+		black: '#000000',
+		primary: '#C67C4E',
+		primaryDark: '#A35F3D',
+		textPrimary: '#2F2D2C',
+		textSecondary: '#9B9B9B',
+	},
+};
